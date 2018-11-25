8e1a23406ae4bdd6cce2fe9fbdf18e0995ce31ca
Sergey Kazakov <tocher@yandex-team.ru>
2018-11-26 01:31:58 +0300

    lab 2

--- client.py
+++ client.py
@@ -1,13 +1,62 @@
-import socket
+import socket, select, string, sys
+import json
 
-HOST = '0.0.0.0'
-PORT = 9999
+def display() :
+	you="\33[33m\33[1m"+" You: "+"\33[0m"
+	sys.stdout.write(you)
+	sys.stdout.flush()
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+def send_json (sock, message):
+	sock.send(bytes(json.dumps(message)))
 
-client.send('hello from client')
+def main():
 
-while True:
-    response = client.recv(4096)
-    print response
+    if len(sys.argv)<2:
+        host = raw_input("Enter host ip address: ")
+    else:
+        host = sys.argv[1]
+
+    port = 5001
+
+    name=raw_input("\33[34m\33[1m CREATING NEW ID:\n Enter username: \33[0m")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+
+    try :
+        s.connect((host, port))
+    except :
+        print "\33[31m\33[1m Can't connect to the server \33[0m"
+        sys.exit()
+
+    send_json(s, name)
+    display()
+    while 1:
+        socket_list = [sys.stdin, s]
+
+        rList, wList, error_list = select.select(socket_list , [], [])
+
+        for sock in rList:
+            #incoming message from server
+            if sock == s:
+                jsonData = sock.recv(4096)
+                try:
+                    data = json.loads(jsonData)
+                except ValueError as e:
+                    print '\33[31m\33[1m \rDISCONNECTED!!\n \33[0m'
+                    sys.exit()
+
+                if not data :
+                    print '\33[31m\33[1m \rDISCONNECTED!!\n \33[0m'
+                    sys.exit()
+                else :
+                    sys.stdout.write(data)
+                    display()
+
+            #user entered a message
+            else :
+                msg=sys.stdin.readline()
+                send_json(s, msg)
+                display()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,45 +1,94 @@
-import socket
-import sys
-from thread import start_new_thread
+import json, socket, select
 
-HOST = ''
-PORT = 9999
+def send_to_all (sock, message):
+	for socket in connected_list:
+		if socket != server_socket and socket != sock :
+			try :
+			    to_json(socket, message)
+			except :
+				# if connection not available
+				socket.close()
+				connected_list.remove(socket)
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-    print("Could not create socket. Error Code: ", str(msg[0]), "Error: ", msg[1])
-    sys.exit(0)
+def to_json (sock, message):
+	sock.send(bytes(json.dumps(message)))
 
-print("[-] Socket Created")
+if __name__ == "__main__":
+	name=""
+	record={}
+	connected_list = []
+	buffer = 4096
+	port = 5001
 
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    s.bind((HOST, PORT))
-    print("[-] Socket Bound to port " + str(PORT))
-except socket.error, msg:
-    print("Bind Failed. Error Code: {} Error: {}".format(str(msg[0]), msg[1]))
-    sys.exit()
+	server_socket.bind(("localhost", port))
+	server_socket.listen(10)
 
-s.listen(10)
-print("Listening...")
+	connected_list.append(server_socket)
 
-def client_thread(conn):
-    conn.send("Welcome to the Server.\n")
+	print "\33[32m \t\t\t\tSERVER WORKING \33[0m"
 
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        print(data)
-        reply = "message from server"
-        conn.sendall(reply)
-    conn.close()
+	while 1:
+		rList,wList,error_sockets = select.select(connected_list,[],[])
 
-while True:
-    conn, addr = s.accept()
-    print("[-] Connected to " + addr[0] + ":" + str(addr[1]))
+		for sock in rList:
+			if sock == server_socket:
+				sockfd, addr = server_socket.accept()
+				jsonData = sockfd.recv(buffer)
+				name = json.loads(jsonData)
+				connected_list.append(sockfd)
+				record[addr]=""
 
-    start_new_thread(client_thread, (conn,))
+				if name in record.values():
+					to_json(sockfd, "\r\33[31m\33[1m Username already taken!\n\33[0m")
+					del record[addr]
+					connected_list.remove(sockfd)
+					sockfd.close()
+					continue
+				else:
+					record[addr]=name
+					print "Client (%s, %s) connected" % addr," [",record[addr],"]"
+					to_json(sockfd, "\33[32m\r\33[1m Welcome to chat room. Enter 'exit' anytime to exit\n\33[0m")
 
-s.close()
+					send_to_all(sockfd, "\33[32m\33[1m\r "+name+" joined the conversation \n\33[0m")
+
+			else:
+				# Data from client
+				try:
+					jsonData = sock.recv(buffer)
+					data1 = json.loads(jsonData)
+
+					#print "sock is: ",sock
+					data=data1[:data1.index("\n")]
+					#print "\ndata received: ",data
+
+					i,p=sock.getpeername()
+					if data == "exit":
+						msg="\r\33[1m"+"\33[31m "+record[(i,p)]+" left the conversation \33[0m\n"
+						send_to_all(sock,msg)
+						print "Client (%s, %s) is offline" % (i,p)," [",record[(i,p)],"]"
+						del record[(i,p)]
+						connected_list.remove(sock)
+						sock.close()
+						continue
+					elif data == "list":
+						msg="\r\33[1m"+"\33[35m Connected users:\33[0m\n"
+						for rec in record:
+						    msg=msg + "\r" + record[(rec[0], rec[1])] + "\n"
+						to_json(sockfd, msg)
+					else:
+						msg="\r\33[1m"+"\33[35m "+record[(i,p)]+": "+"\33[0m"+data+"\n"
+						send_to_all(sock,msg)
+
+                #abrupt user exit
+				except:
+					(i,p)=sock.getpeername()
+					send_to_all(sock, "\r\33[31m \33[1m"+record[(i,p)]+" left the conversation unexpectedly\33[0m\n")
+					print "Client (%s, %s) is offline (error)" % (i,p)," [",record[(i,p)],"]\n"
+					del record[(i,p)]
+					connected_list.remove(sock)
+					sock.close()
+					continue
+
+	server_socket.close()
